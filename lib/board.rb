56830db67618d6d4513aa792cda7bc6929ad8255
PlaustralCL <plaustralcl@gmail.com>
2021-10-14 07:05:26 -0400

    Add methods to get columns and rows
    Added method to get the columns and rows from the gameboard, rather
    than repeating gameboard.values.transpose multiple times.

--- lib/board.rb
+++ lib/board.rb
@@ -19,28 +19,36 @@ class Board
     end
   end
 
-  def full_board?(columns = gameboard.values)
+  def full_board?(columns = gameboard_colums)
     columns.flatten.none?(/[1-6]/)
   end
 
-  def column_winner(columns = gameboard.values)
+  def column_winner(columns = gameboard_colums)
     check_for_winner(columns)
   end
 
-  def row_winner(rows = gameboard.values.transpose)
+  def row_winner(rows = gameboard_rows)
     check_for_winner(rows)
   end
 
-  def diagonal_winner(matrix = gameboard.values.transpose)
+  def diagonal_winner(matrix = gameboard_rows)
     check_for_winner(diagonals(matrix))
     check_for_winner(anti_diagonals(matrix))
   end
 
-  def check_for_winner(columns = gameboard.values)
+  def check_for_winner(columns = gameboard_colums)
     columns.each do |col|
       return @winner = "X" if col.join.include?("XXXX")
       return @winner = "O" if col.join.include?("OOOO")
     end
+  end
+
+  def gameboard_colums
+    gameboard.values
+  end
+
+  def gameboard_rows
+    gameboard.values.transpose
   end
 
 end
